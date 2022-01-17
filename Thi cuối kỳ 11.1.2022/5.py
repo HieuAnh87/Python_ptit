a632fc7f157ca50ea2fb737efcf7be917ada5cea
HieuAnh87 <86817199+HieuAnh87@users.noreply.github.com>
2022-01-17 14:18:03 +0700

    Delete Thi cuối kỳ 11.1.2022 directory

--- Thi cuối kỳ 11.1.2022/5.py
+++
@@ -1,8 +0,0 @@
-from itertools import accumulate
-import math
-n = int(input())
-a = [int(i) for i in input().split()]
-res = list(accumulate(a))
-tong = sum(res)
-tich = math.prod(res)
-print(tong,tich)
